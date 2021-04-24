2bdcf0ae23ca4aa23db513baad51dd0fd140490d
CrookiNari <38683641+CrookiNari@users.noreply.github.com>
2021-04-24 13:58:50 +1000

    Add files via upload

---
+++ maps/Plain of Andor/aj24.lua
@@ -0,0 +1,39 @@
+-- Lua script of map aj24.
+-- This script is executed every time the hero enters this map.
+
+-- Feel free to modify the code below.
+-- You can add more events and remove the ones you don't need.
+
+-- See the Solarus Lua API documentation:
+-- http://www.solarus-games.org/doc/latest
+
+local map = ...
+local game = map:get_game()
+
+-- Event called at initialization time, as soon as this map becomes is loaded.
+function map:on_started()
+
+  -- You can initialize the movement and sprites of various
+  -- map entities here.
+function thirstylady:on_interaction()
+
+game:start_dialog ("thirstylady.1", function(answer)
+  if answer == 1 then -- Yes.
+  game:start_dialog("thirstylady.2")
+  else 
+  game:start_dialog("thirstylady.3")
+end
+end)
+
+end
+
+-- Event called after the opening transition effect of the map,
+-- that is, when the player takes control of the hero.
+function map:on_opening_transition_finished()
+
+end
+
+end
+
+-- Stuff to finish:
+-- add thirsty lady quest
